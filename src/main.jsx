13f9304febf04c91b180ab70fec4effb58842fee
Oranyelu <georgechime91@gmail.com>
2025-01-09 13:29:42 +0100

    rebuild

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/LandingPage';
+import About from './pages/About';
+import Contact from './pages/Contacts';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<div>Page Not Found</div>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+);
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaGithub, FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
 import ProfileImage from "../assets/user profile 1.jpg";
